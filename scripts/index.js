030e1f258c38162dcef1fff7bbcc7505c4cf172a
ChichoRomero <e.romerodehaz@gmail.com>
2023-08-11 16:46:59 +0300

    Change window location method

--- scripts/index.js
+++ scripts/index.js
@@ -169,7 +169,7 @@ function search() {
 
     localStorage.setItem('results', JSON.stringify(results))
 
-    window.location.replace("../views/results.html")
+    window.location.assign("/views/results.html")
 }
 
 document.getElementById("view-cart").addEventListener("click", viewCart)
