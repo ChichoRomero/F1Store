792fc6d4e884d94a5bc9486e9841bac4ff6ededb
ChichoRomero <e.romerodehaz@gmail.com>
2023-08-11 17:08:24 +0300

    Reverted changes in search functionality

--- scripts/index.js
+++ scripts/index.js
@@ -169,7 +169,7 @@ function search() {
 
     localStorage.setItem('results', JSON.stringify(results))
 
-    window.location.assign("/views/results.html")
+    window.location.replace("../views/results.html")
 }
 
 document.getElementById("view-cart").addEventListener("click", viewCart)
